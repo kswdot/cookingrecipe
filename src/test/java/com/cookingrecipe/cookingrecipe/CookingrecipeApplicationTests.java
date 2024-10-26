74d26446879276896d768f8b163e5b5aff4fd08e
kswdot <rlatjddms7a@naver.com>
2024-10-27 03:25:24 +0900

    Initial commit

---
+++ src/test/java/com/cookingrecipe/cookingrecipe/CookingrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookingrecipe.cookingrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
